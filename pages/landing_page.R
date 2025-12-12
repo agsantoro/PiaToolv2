02969535bc448c823f1fba862d3ab36138342f68
Federico Rolón <frolon@iecs.org.ar>
2025-12-12 14:46:56 -0300

    Arreglé algunas descripciones de las intervenciones en el landings.

--- pages/landing_page.R
+++ pages/landing_page.R
@@ -298,21 +298,21 @@ background: #ffffff !important;
         ),
         menuBox(
           title = "Tratamiento para la hepatitis C crónica",
-          text = "El modelo del uso de tratamiento específico para Hepatitis C Crónica le permite evaluar el impacto del uso del mismo en personas ya diagnosticadas, con distintos estadíos de fibrosis hepática y que nunca han recibido tratamiento anteriormente, en la carga de enfermedad por Hepatitis C Crónica. Mediante la modificación de parámetros como la efectividad del tratamiento antiviral y los costos del mismo, podrá calcular indicadores como la cantidad de cirrosis, de carcinomas hepatocelulares y de muertes evitadas, el costo total de la intervención y el retorno de inversión (ROI).",
+          text = "El modelo del uso de tratamiento específico para Hepatitis C Crónica le permite evaluar el impacto del uso del mismo en personas ya diagnosticadas, con distintos estadíos de fibrosis hepática y que nunca han recibido tratamiento anteriormente, en la carga de enfermedad por Hepatitis C Crónica.",
           iconType = "virus",
           iconColor = "#2C5F8B",
           linkTo = "hepC"
         ),
         menuBox(
           title = "Uso de oxitocina para la prevención de la hemorragia post parto",
-          text = "El modelo del uso de Oxitocina para la prevención de Hemorragia Post Parto permite evaluar el impacto del aumento de cobertura del uso de oxitocina durante el parto en la carga de enfermedad por hemorragia postparto modificando parámetros como el porcentaje de uso de oxitocina y el costo de la misma. Con este modelo podrá calcular indicadores como la cantidad de hemorragias postparto evitadas, las muertes evitadas, el costo total de la intervención y el retorno de inversión (ROI).",
+          text = "El modelo del uso de Oxitocina para la prevención de Hemorragia Post Parto permite evaluar el impacto del aumento de cobertura del uso de oxitocina durante el parto en la carga de enfermedad por hemorragia postparto modificando parámetros como el porcentaje de uso de oxitocina y el costo de la misma.",
           iconType = "baby",
           iconColor = "#2C5F8B",
           linkTo = "hpp"
         ),
         menuBox(
           title = "Profilaxis pre exposición (PrEP) para VIH",
-          text = "El modelo de PrEP permite evaluar el impacto del uso de profilaxis pre exposición oral en personas con alto riesgo de infección por el Virus de la Inmunodeficiencia Humana (VIH) en la carga de enfermedad por esta infección modificando parámetros como el porcentaje de adherencia a la medicación y el costo anual del uso de PrEP. Con este modelo podrá obtener indicadores como la cantidad de casos de VIH evitados, las muertes evitadas, el costo total de la intervención y el retorno de inversión (ROI).",
+          text = "El modelo de PrEP permite evaluar el impacto del uso de profilaxis pre exposición oral en personas con alto riesgo de infección por el Virus de la Inmunodeficiencia Humana (VIH) en la carga de enfermedad por esta infección modificando parámetros como el porcentaje de adherencia a la medicación y el costo anual del uso de PrEP.",
           iconType = "pills",
           iconColor = "#2C5F8B",
           linkTo = "prep"
